f290237c90221779796395114a9e7015991e26d8
0869922729zx <0869922729zx@gmail.com>
2023-05-15 16:17:18 +0700

    add project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.c1dtnubgxgb8.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::my-bucket-uda
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100 
